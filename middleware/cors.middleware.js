ecc947c704de00a1f818700ccd279a40ef754e05
bauerindustries <104348398+bauerindustries@users.noreply.github.com>
2022-11-17 21:29:16 +0000

    Update cors.middleware.js

--- middleware/cors.middleware.js
+++ middleware/cors.middleware.js
@@ -1,5 +1,8 @@
 function enablecCors(req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://cute-peony-3eb499.netlify.app'
+  );
   res.setHeader(
     'Access-Control-Allow-Methods',
     'GET,POST,PATCH,DELETE,OPTIONS'
